20ac5ee8a06a616c8a3bbcff6a9f5ef0ec48a2bc
megan <meganambermcbride@gmail.com>
2022-06-22 13:59:38 -0400

    Reformat

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
 
     // Checks if over or under cmax count
     if (remainingCharacters < 0) {
-
       counterAmount.addClass('tweet-is-too-long')
     } else {
       counterAmount.removeClass('tweet-is-too-long')
@@ -23,34 +22,3 @@ $(document).ready(function() {
   });
 
 });
-
-
-// const { render } = require("express/lib/application");
-
-// $(document).ready(function() {
-
-//   // On input within textarea run function below
-//   $('#textarea').on('input', function (event) {
-    
-//     let $formInput = $(this);
-//     let $forms = $formInput.closest("form");
-//     let $textCount = $forms.find("#tweet-text");
-    
-//     // Checks the length of whats being written
-//     let inputCharacterCount = $textCount.val().length;
-
-//     // Character count left
-//     let remainingCharacters = 140 - inputCharacterCount;
-    
-//     // Displays the remaining character
-//     let counterColour = $("#counter").val(remainingCharacters)
-
-//     // Checks if over or under cmax count
-//     if (remainingCharacters < 0) {
-//       counterColour.css("color", "red")
-//     } else {
-//       counterColour.css("color", "white")
-//     }
-//   });
-
-// });
