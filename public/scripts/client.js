0b9a68edb459a5dffd8d8828dc128c470d90a82b
megan <meganambermcbride@gmail.com>
2022-06-23 14:47:37 -0400

    Add error mesage display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,12 @@
 
 $(document).ready(function() {
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       $('.tweets-column').prepend(createTweetElement(tweet));
@@ -16,13 +22,13 @@ $(document).ready(function() {
     let $tweet = $(` 
   <article class="tweet-container">
   <header class="tweet-container-header">
-  <img class= "tweet-profile-image" src=${tweet.user.avatars}>
-  <span class="tweet-name"> ${tweet.user.name} </span>
-  <span class="username"> ${tweet.user.handle}</span>
+  <img class= "tweet-profile-image" src=${escape(tweet.user.avatars)}>
+  <span class="tweet-name"> ${escape(tweet.user.name)} </span>
+  <span class="username"> ${escape(tweet.user.handle)}</span>
   </header>
-  <article class="previous-tweet-text">${tweet.content.text}</article>
+  <article class="previous-tweet-text">${escape(tweet.content.text)}</article>
   <footer class="tweet-container-bottom">
-  <span> ${timeago.format(tweet.created_at)}</span>
+  <span> ${escape(timeago.format(tweet.created_at))}</span>
   <section class="tweet-icons">
   <i class="fa-solid fa-flag"></i>
   <i class="fa-solid fa-retweet"></i>
@@ -38,12 +44,34 @@ $(document).ready(function() {
   $('form').submit(async (event) => {
     event.preventDefault();
 
+    let textInput = $("#tweet-text").val();
+
+
+    $('.tweet-error').slideUp()
+    if (!textInput) {
+      return $('.tweet-error').text('Please enter text').slideDown()
+  
+    }
+
+    if (textInput.length >140) {
+      return $('.tweet-error').text('Tweet exceeds maximum characters').slideDown()
+    }
+
+
+
     await
       $.ajax("/tweets/", {
         method: "POST",
         url: "/tweets/",
         data: $('#form-section').serialize()
-      });
+      })
+      .then(function(tweet) {
+        loadTweets()
+      })
+      .catch((err) => {
+        console.error("There was an error: ", err)
+      })
+
   });
 
   const loadTweets = function() {
@@ -55,8 +83,5 @@ $(document).ready(function() {
     });
 
   };
-
-  loadTweets();
-
 });
 
